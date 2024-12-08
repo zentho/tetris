437bd7b4c5eb598cad59080684d7a1fb1b989062
zentho <102931585+zentho@users.noreply.github.com>
2024-12-07 17:43:55 -0800

    rename patterns to shapes

--- tetris.py
+++ tetris.py
@@ -3,12 +3,11 @@ import random
 
 pygame.init()
 W, H, S = 400, 800, 40
-
 window = pygame.window.set_mode((W, H))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Tetris")
 
-patterns = [
+shapes = [
     [[1, 1, 1, 1]],
     [[0, 1, 1],
      [1, 1, 0]],
