3e05e402131f0eff7aed60f415a4c09abacf2e8d
zentho <102931585+zentho@users.noreply.github.com>
2024-12-07 21:38:18 -0800

    add collides function

--- tetris.py
+++ tetris.py
@@ -34,5 +34,27 @@ def new_piece():
     return [random.choice(shapes), [W//S//2-2, 1], random_color()]
 
 
-p = new_piece
+piece = new_piece()
 board = [[0] * (W//S) for _ in range(H//S)]
+
+
+def collides(piece, board):
+    shape, location, color = piece
+    x, y = location
+
+    for r, row in shape:
+        for c, cell in row:
+            invalid_x = x + r < 0 or len(board[0]) <= x + r
+            invalid_y = y + c < 0 or len(board) <= y + c
+            if invalid_x or invalid_y or cell:
+                return True
+
+    return False
+
+
+def place(piece, board):
+    pass
+
+
+def clear(board):
+    pass
