d59d75dd8c92d5e2f744d683d692a77da9977bbe
zentho <102931585+zentho@users.noreply.github.com>
2024-12-07 16:47:49 -0800

    add piece patterns

--- tetris.py
+++ tetris.py
@@ -2,9 +2,24 @@ import pygame
 import random
 
 pygame.init()
-
 W, H, S = 400, 800, 40
 
 window = pygame.window.set_mode((W, H))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Tetris")
+
+patterns = [
+    [[1, 1, 1, 1]],
+    [[0, 1, 1],
+     [1, 1, 0]],
+    [[1, 1, 0],
+     [0, 1, 1]]
+    [[0, 1, 0],
+     [1, 1, 1]],
+    [[1, 1, 1],
+     [0, 0, 1]],
+    [[1, 1, 1],
+     [1, 0, 0]],
+    [[1, 1],
+     [1, 1]]
+]
